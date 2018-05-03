10fecc4f9bdf49b15e7c7060de5280d8698f4695
Sebastian Franco Ulloa <cebasfu93@gmail.com>
2018-05-03 10:03:26 +0200

    Takes arguments to be more general

--- density.py
+++ density.py
@@ -1,4 +1,5 @@
 import numpy as np
+from optparse import OptionParser
 import matplotlib.pyplot as plt
 from matplotlib import rc, rcParams
 rc('font',family='Times New Roman')
@@ -7,9 +8,14 @@ Z=38
 rc('text', usetex=True)
 rcParams.update({'font.size': Z})
 
-tails=np.genfromtxt("../POPC3-17_PRO1_dens_tails.xvg", skip_header=23)
-heads=np.genfromtxt("../POPC3-17_PRO1_dens_heads.xvg", skip_header=23)
-sol=np.genfromtxt("../POPC3-17_PRO1_dens_sol.xvg", skip_header=23)
+parser=OptionParser()
+parser.add_option("-f", "--prefix", action="store", type="string", dest="Name", default="NP1", help="Prefix name of the files containing the densities")
+(options, args)= parser.parse_args()
+name_opt=options.Name
+
+tails=np.genfromtxt(name_opt+"_dens_tails.xvg", skip_header=23)
+heads=np.genfromtxt(name_opt+"_dens_heads.xvg", skip_header=23)
+sol=np.genfromtxt(name_opt+"_dens_sol.xvg", skip_header=23)
 tails[:,0] = tails[:,0] - np.max(tails[:,0])/2
 heads[:,0] = heads[:,0] - np.max(heads[:,0])/2
 sol[:,0] = sol[:,0] - np.max(sol[:,0])/2
@@ -23,5 +29,5 @@ plt.xlim((0,1200))
 plt.xticks([0, 500, 1000])
 #plt.grid()
 plt.gca().invert_xaxis()
-plt.savefig("POPC3-17_density.png", format="png", dpi=300, bbox_extra_artists=(lgd,), bbox_inches='tight')
+plt.savefig("Multimedia/"+name_opt+"_density.png", format="png", dpi=300, bbox_extra_artists=(lgd,), bbox_inches='tight')
 plt.close()
